51b888d49c466eee01a6411bbe6977978d7b81c5
nish <nishant.kohli@storydigital.in>
2021-04-06 00:36:20 +0530

    remove page2

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -3,14 +3,12 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Sample from '@Components/Sample';
 
 const Page404 = lazy(() => import('@Pages/Page404'));
-const Page2 = lazy(() => import('@Pages/Page2'));
 
 const Routing = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Sample} />
-                <Route path="/page2" component={Page2} />
                 <Route component={Page404} />
             </Switch>
         </BrowserRouter>
--- src/pages/Page2.js
+++
@@ -1,9 +0,0 @@
-const Page2 = () => {
-    return (
-        <div>
-            <p className="text-red-500 font-semibold"> This is page 2. </p>
-        </div>
-    );
-};
-
-export default Page2;
